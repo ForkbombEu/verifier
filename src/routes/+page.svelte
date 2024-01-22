0caa9906c549496731e8f199adb946dbc650eeaf
phoebus-84 <ennio@dyne.org>
2024-01-22 02:44:29 +0100

    feat: qr code generation and push notifications

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,60 @@
 	import Logo from '$lib/components/atoms/Logo.svelte';
 	import Card from '$lib/components/molecules/Card.svelte';
 	import fakeCredentials from '$lib/fakeCredentials';
+	//
+	// import {
+	// 	PushNotifications,
+	// 	type DeliveredNotifications,
+	// 	type PushNotificationSchema
+	// } from '@capacitor/push-notifications';
+
+	// let incomingNotification: PushNotificationSchema;
+	// let registrationToken: string;
+	// let notificationsList: DeliveredNotifications;
+
+	// const addListeners = async () => {
+	// 	await PushNotifications.addListener('registration', (token) => {
+	// 		registrationToken = token.value;
+	// 		console.info('Registration token: ', token.value);
+	// 	});
+
+	// 	await PushNotifications.addListener('registrationError', (err) => {
+	// 		registrationToken = err.error;
+	// 		console.error('Registration error: ', err.error);
+	// 	});
+
+	// 	await PushNotifications.addListener('pushNotificationReceived', (notification) => {
+	// 		incomingNotification = notification;
+	// 		console.log('Push notification received: ', notification);
+	// 	});
+
+	// 	await PushNotifications.addListener('pushNotificationActionPerformed', (notification) => {
+	// 		console.log('Push notification action performed', notification.actionId, notification.inputValue);
+	// 	});
+	// };
+
+	// const registerNotifications = async () => {
+	// 	let permStatus = await PushNotifications.checkPermissions();
+
+	// 	if (permStatus.receive === 'prompt') {
+	// 		permStatus = await PushNotifications.requestPermissions();
+	// 	}
+
+	// 	if (permStatus.receive !== 'granted') {
+	// 		throw new Error('User denied permissions!');
+	// 	}
+
+	// 	await PushNotifications.register();
+	// };
+
+	// const getDeliveredNotifications = async () => {
+	// 	const notificationList = await PushNotifications.getDeliveredNotifications();
+	// 	console.log('delivered notifications', notificationList);
+	// };
+
+	// registerNotifications();
+	// addListeners();
+	// $: getDeliveredNotifications();
 </script>
 
 <ion-header>
@@ -17,9 +71,23 @@
 
 <ion-content fullscreen class="ion-padding space-y-10">
 	<h1 class="text-[32px] font-medium leading-[20.5px] tracking-[-0.5px] text-[#E8D2FF]">Home</h1>
-{#each fakeCredentials as credential}
-	<Card title={credential.title} content={credential.issuedBy}>
-		<ion-button href={`/verify/`}>Verify</ion-button>
-	</Card>
-{/each}
+	{#each fakeCredentials as credential}
+		<Card title={credential.title} content={credential.issuedBy}>
+			<ion-button href={`/verify/`}>Verify</ion-button>
+		</Card>
+	{/each}
+	<br />
+	<!-- {#if registrationToken}
+		<ion-Input value={registrationToken} label="registration token" />
+	{/if}
+	{#if incomingNotification}
+		<pre>
+				{JSON.stringify(incomingNotification, null, 2)}
+			</pre>
+	{/if}
+	{#if notificationsList}
+		<pre>
+				{JSON.stringify(notificationsList, null, 2)}
+			</pre>
+	{/if} -->
 </ion-content>
--- src/routes/verify/+page.svelte
+++ src/routes/verify/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import Logo from '$lib/components/atoms/Logo.svelte';
+	import { PushNotifications } from '@capacitor/push-notifications';
 	//@ts-ignore
 	import { Slangroom } from '@slangroom/core';
 	//@ts-ignore
 	import { qrcode } from '@slangroom/qrcode';
+	import { onMount } from 'svelte';
+
+	let qr: any;
+	let error: string;
+
 	const scriptCreate = `
 Rule unknown ignore
 
@@ -12,11 +18,46 @@ Given I have a 'string' named 'qrcode'
 Then print data
 `;
 	const slangroom = new Slangroom(qrcode);
-	const promise = slangroom.execute(scriptCreate, {
-		data: {
-			text: 'i am a pony!'
+
+	const addListeners = async () => {
+		await PushNotifications.addListener('registration', async (token) => {
+			const text = JSON.stringify({
+				url: 'http://localhost:3000',
+				name: 'over18',
+				issuedBy: 'ItGov',
+				registrationToken: token.value
+			});
+			qr = await slangroom.execute(scriptCreate, {
+				data: {
+					text
+				}
+			});
+		});
+
+		await PushNotifications.addListener('registrationError', (err) => {
+			error = err.error;
+		});
+	};
+
+	const registerNotifications = async () => {
+		let permStatus = await PushNotifications.checkPermissions();
+
+		if (permStatus.receive === 'prompt') {
+			permStatus = await PushNotifications.requestPermissions();
 		}
+
+		if (permStatus.receive !== 'granted') {
+			throw new Error('User denied permissions!');
+		}
+
+		await PushNotifications.register();
+	};
+	onMount(async () => {
+		await registerNotifications();
+		await addListeners();
 	});
+	registerNotifications();
+	addListeners();
 </script>
 
 <ion-header>
@@ -32,12 +73,7 @@ Then print data
 
 <ion-content fullscreen class="ion-padding">
 	<ion-button href="/">back</ion-button>
-
-	{#await promise}
-		waiting
-	{:then res}
-		<img src={res.result.qrcode} class="w-full pt-20" />
-	{:catch error}
-		Something bad happened.
-	{/await}
+	{#if qr}
+		<img src={qr.result.qrcode} alt="qrCode" class="w-full pt-20" />
+	{/if}
 </ion-content>
