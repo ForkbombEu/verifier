77303fbf21f63f214a93b1b1593b033ebeb13ac6
phoebus-84 <ennio@dyne.org>
2024-01-24 17:56:59 +0100

    feat: import didroom-components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
 	import { setupIonicBase } from 'ionic-svelte';
+
 	setupIonicBase();
+
 	import '@fontsource/poppins';
 	import 'ionic-svelte/components/all';
 	import '../theme/variables.css';
 </script>
+
+<svelte:head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0" />
+	<script
+		type="module"
+		src="https://cdn.jsdelivr.net/npm/@didroom/components/dist/didroom-components/didroom-components.esm.js"
+	></script>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/@didroom/components/dist/didroom-components/didroom-components.css"
+	/>
+</svelte:head>
 
 <ion-app>
 	<slot />
