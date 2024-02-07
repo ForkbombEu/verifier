66ab03a72ca3d71af555406f99f9e494c77884e4
phoebus-84 <ennio@dyne.org>
2024-02-07 16:43:21 +0100

    feat: header component

--- src/routes/[id]/credential-detail/+page.svelte
+++ src/routes/[id]/credential-detail/+page.svelte
@@ -1,23 +1,13 @@
 <script lang="ts">
-	import Logo from '$lib/components/atoms/Logo.svelte';
-
+	import Header from '$lib/components/atoms/Header.svelte';
 	export let data: any;
 	const { credential } = data;
 </script>
 
-<ion-header>
-	<ion-toolbar>
-		<ion-title>
-			<div class="flex items-center gap-2">
-				<Logo />
-				<h1 class="text-2xl">Credential detail</h1>
-			</div>
-		</ion-title>
-	</ion-toolbar>
-</ion-header>
+<Header>CREDENTIAL DETAIL</Header>
 <ion-content fullscreen class="ion-padding">
 	<d-credential-card {...credential} expiration-date="2017-01-01" verified="false"> </d-credential-card>
-	<div class="fixed w-full bottom-0 left-0 bg-tab pb-11">
+	<div class="fixed w-full bottom-0 left-0 bg-tab">
 		<d-credential-detail {...credential} description={credential.expand.templates[0].name} href={`/${credential.id}/verify/`} />
 	</div>
 </ion-content>
