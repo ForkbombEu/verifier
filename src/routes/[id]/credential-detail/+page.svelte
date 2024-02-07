fa125a6e2a95b5f9811ab194824044f798bfa770
phoebus-84 <ennio@dyne.org>
2024-02-07 16:46:00 +0100

    fix: credential detail card position

--- src/routes/[id]/credential-detail/+page.svelte
+++ src/routes/[id]/credential-detail/+page.svelte
@@ -17,7 +17,7 @@
 </ion-header>
 <ion-content fullscreen class="ion-padding">
 	<d-credential-card {...credential} expiration-date="2017-01-01" verified="false"> </d-credential-card>
-	<div class="fixed w-full bottom-0 left-0 bg-tab pb-11">
+	<div class="fixed w-full bottom-0 left-0 bg-tab">
 		<d-credential-detail {...credential} description={credential.expand.templates[0].name} href={`/${credential.id}/verify/`} />
 	</div>
 </ion-content>
