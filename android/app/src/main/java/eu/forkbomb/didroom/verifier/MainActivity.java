f4abb6f9956bb7e20334bad1ecff1faaa6b6cd39
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-12 16:41:36 +0100

    remove everything

--- android/app/src/main/java/eu/forkbomb/didroom/verifier/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package eu.forkbomb.didroom.verifier;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/routes/[id]/+layout.ts
+++
@@ -1,6 +0,0 @@
-import { getService } from '$lib/slangroom/services.js';
-
-export const load = async ({ params }) => {
-	const credential = await getService(params.id);
-	return { credential };
-};
