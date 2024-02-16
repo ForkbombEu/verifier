f38ee39aff3595b7d11b58e5bb3569b9d0f99703
phoebus-84 <ennio@dyne.org>
2024-02-16 14:42:31 +0100

    fix: tailwind theme from shared components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,19 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: 'Poppins'
+				sans: ['"Gantari Variable"', 'sans-serif'],
+				icon: ['"Material Symbols Rounded"']
+			},
+			colors: {
+				surface: 'var(--surface)',
+				primary: 'var(--primary)',
+				on: 'var(--on)',
+				'on-alt': 'var(--on-alt)',
+				accent: 'var(--accent)',
+				'on-accent': 'var(--on-accent)',
+				success: 'var(--success)',
+				warning: 'var(--warning)',
+				error: 'var(--error)'
 			}
 		}
 	},
