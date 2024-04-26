61447cd3b06f938a4880454c432625944a1b76f8
phoebus-84 <ennio@dyne.org>
2024-04-26 17:52:16 +0200

    feat: fetch verification flows

---
+++ src/lib/slangroom/verificationFlows.ts
@@ -0,0 +1,79 @@
+import { backendUri } from '$lib/backendUri';
+import { Slangroom } from '@slangroom/core';
+import getPbList from '$lib/slangroom/getPbList.zen?raw';
+import { pocketbase } from '@slangroom/pocketbase';
+
+export type VerificationFlow = {
+	collectionId: string;
+	collectionName: string;
+	created: string;
+	description: string;
+	expand: Expand;
+	id: string;
+	name: string;
+	organization: string;
+	public: boolean;
+	relying_party: string;
+	template: string;
+	updated: string;
+};
+
+export type Expand = {
+	organization: Organization;
+	relying_party: RelyingParty;
+};
+
+export type Organization = {
+	avatar: string;
+	collectionId: string;
+	collectionName: string;
+	created: string;
+	description: string;
+	id: string;
+	name: string;
+	updated: string;
+};
+
+export type RelyingParty = {
+	collectionId: string;
+	collectionName: string;
+	created: string;
+	endpoint: string;
+	id: string;
+	name: string;
+	organization: string;
+	updated: string;
+};
+export type PaginatedResult<T> = {
+	page: number;
+	perPage: number;
+	totalItems: number;
+	totalPages: number;
+	items: T[];
+};
+
+export type Response<T> = {
+	result: T;
+	status: number;
+};
+
+const slangroom = new Slangroom(pocketbase);
+
+export const getVerificationFlows = async (): Promise<VerificationFlow[]> => {
+	try {
+		const data = {
+			pb_address: backendUri,
+			list_parameters: {
+				collection: 'verification_flows',
+				expand: 'relying_party, organization',
+				sort: '-updated',
+				type: 'all'
+			}
+		};
+		const res = await slangroom.execute(getPbList, { data });
+		//@ts-expect-error output needs to be typed
+		return res.result?.output?.records;
+	} catch (e: unknown) {
+		throw new Error(JSON.stringify(e));
+	}
+};
