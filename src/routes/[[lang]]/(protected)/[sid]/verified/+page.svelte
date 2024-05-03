753ca95bf1911c2e4be00af28c0b07d7a727c906
phoebus-84 <ennio@dyne.org>
2024-05-03 15:46:54 +0200

    feat: end of verification flow

---
+++ src/routes/[[lang]]/(protected)/[sid]/verified/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Header from '$lib/components/molecules/Header.svelte';
+	import type { Feedback } from '$lib/utils/types.js';
+	import dayjs from 'dayjs';
+
+	export let data;
+
+	const { verifiedSid } = data;
+	const { sid, at, success, message } = verifiedSid;
+	let feedback: Feedback;
+	$: if (!success) {
+		feedback = {
+			type: 'error',
+			feedback: 'Verification failed',
+			message
+		};
+	}
+</script>
+
+<Header>Verification</Header>
+<ion-content class="ion-padding">
+	<d-feedback {...feedback} />
+	<div class="flex w-full flex-row items-start justify-around">
+		<d-session-card {sid} date={dayjs(at).toString()} {success} />
+	</div>
+</ion-content>
