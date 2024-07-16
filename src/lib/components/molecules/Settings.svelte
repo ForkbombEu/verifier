ac71414e421bfbc69e3731f58291c567cb436eb1
phoebus-84 <ennio@dyne.org>
2024-07-16 16:04:27 +0200

    chores: update profile/organizations components

--- src/lib/components/molecules/Settings.svelte
+++ src/lib/components/molecules/Settings.svelte
@@ -1,17 +1,18 @@
 <script>
-	import { page } from "$app/stores";
-	import { goto, i18n } from "$lib/i18n";
-	import { setLanguagePreference } from "$lib/preferences/lang";
-	import { availableLanguageTags } from "$paraglide/runtime";
-
+	import { page } from '$app/stores';
+	import { goto, i18n } from '$lib/i18n';
+	import { setLanguagePreference } from '$lib/preferences/lang';
+	import { availableLanguageTags } from '$paraglide/runtime';
+	import AppDetails from '../AppDetails.svelte';
 </script>
+
 <div class="flex flex-col gap-2">
-    <hr />
+	<hr />
 	<d-heading size="xs">Language</d-heading>
 	<ion-radio-group
 		value={i18n.getLanguageFromUrl($page.url)}
 		on:ionChange={async (e) => {
-            await setLanguagePreference(e.detail.value);
+			await setLanguagePreference(e.detail.value);
 			await goto(i18n.route($page.url.pathname), e.detail.value);
 		}}
 	>
@@ -20,5 +21,6 @@
 			<br />
 		{/each}
 	</ion-radio-group>
-    <hr />
+	<hr />
+	<AppDetails />
 </div>
