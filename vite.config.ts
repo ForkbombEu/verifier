b3cac452657205f7c27d246ec3811c3d80fe4506
phoebus-84 <ennio@dyne.org>
2024-09-04 16:31:29 +0200

    build: edit maximumFileSizeToCacheInBytes and add a chuncks visulizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vitest/config';
 import { paraglide } from '@inlang/paraglide-js-adapter-sveltekit/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
 	plugins: [
@@ -9,8 +10,16 @@ export default defineConfig({
 			project: './project.inlang',
 			outdir: './src/paraglide'
 		}),
+		visualizer({
+			emitFile: true,
+			filename: 'stats.html'
+		}),
 		sveltekit(),
-		SvelteKitPWA()
+		SvelteKitPWA({
+			workbox: {
+				maximumFileSizeToCacheInBytes: 5 * 1024 ** 2
+			}
+		})
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
