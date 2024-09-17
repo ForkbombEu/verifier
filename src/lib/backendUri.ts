2884ad64457fe45a93cae8591e54418ec1a65f98
phoebus-84 <ennio@dyne.org>
2024-09-17 09:44:10 +0200

    fix: use staging.didroom in test envoiroment

--- src/lib/backendUri.ts
+++ src/lib/backendUri.ts
@@ -4,7 +4,7 @@ const api = '/api';
 const files = `${api}/files`;
 const userCollectionId = '_pb_users_auth_';
 
-export const backendUri = PUBLIC_BACKEND_URL || 'https://admin.didroom.com';
+export const backendUri = PUBLIC_BACKEND_URL || 'https://staging.admin.didroom.com';
 export const filesUri = (fileName: string, collection: string, id: string) =>
 	`${backendUri}${files}/${collection}/${id}/${fileName}`;
 export const authFilesUri = (fileName: string | undefined, userId: string | undefined) =>
